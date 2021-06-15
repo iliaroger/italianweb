3d94b4ea42289188320b814bd820e25f005b83ae
Ilia Roger <iliailia@me.com>
2021-06-16 01:27:53 +0200

    minor changes

--- bioeticgoods/components/Partners.tsx
+++ bioeticgoods/components/Partners.tsx
@@ -7,24 +7,42 @@ export default function Partners() {
         <p className="text-gray-600 font-light text-lg tracking-wide text-center">
           Our suppliers who deliver quality food
         </p>
-        <img className="w-24 cursor-pointer" src="/images/airbnbLogo.svg"></img>
-        <img
-          className="w-24 cursor-pointer"
-          src="/images/producthuntLogo.svg"
-        ></img>
-        <img
-          className="w-24 cursor-pointer"
-          src="/images/instagramLogo.svg"
-        ></img>
-        <img
-          className="w-24 cursor-pointer"
-          src="/images/facebookLogo.svg"
-        ></img>
-        <img
-          className="w-24 cursor-pointer"
-          src="/images/spotifyLogo.svg"
-        ></img>
-        <img className="w-24 cursor-pointer" src="/images/googleLogo.svg"></img>
+        <a target="_blank" href="https://www.airbnb.com">
+          <img
+            className="w-24 cursor-pointer"
+            src="/images/airbnbLogo.svg"
+          ></img>
+        </a>
+        <a target="_blank" href="https://www.producthunt.com">
+          <img
+            className="w-24 cursor-pointer"
+            src="/images/producthuntLogo.svg"
+          ></img>
+        </a>
+        <a target="_blank" href="https://www.instagram.com">
+          <img
+            className="w-24 cursor-pointer"
+            src="/images/instagramLogo.svg"
+          ></img>
+        </a>
+        <a target="_blank" href="https://www.facebook.com">
+          <img
+            className="w-24 cursor-pointer"
+            src="/images/facebookLogo.svg"
+          ></img>
+        </a>
+        <a target="_blank" href="https://www.spotify.com">
+          <img
+            className="w-24 cursor-pointer"
+            src="/images/spotifyLogo.svg"
+          ></img>
+        </a>
+        <a target="_blank" href="https://www.google.com">
+          <img
+            className="w-24 cursor-pointer"
+            src="/images/googleLogo.svg"
+          ></img>
+        </a>
       </div>
     </div>
   );
