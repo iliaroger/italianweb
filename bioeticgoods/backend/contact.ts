9d6dfa7e36072d45df9c3b55373d1420d966baad
Ilia Roger <iliailia@me.com>
2021-07-06 02:41:44 +0200

    minor changes

--- bioeticgoods/backend/contact.ts
+++ bioeticgoods/backend/contact.ts
@@ -11,7 +11,7 @@ export const sendEmail = async (
   try {
     const res = await axios({
       method: 'post',
-      url: '/api/email',
+      url: 'https://bioeticgoods.iliaroger.de/api/email',
       headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Credentials': true,
