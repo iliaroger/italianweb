b97c68d57366d28ed93e7ca75840d02d5de949b4
Ilia Roger <iliailia@me.com>
2021-07-06 21:49:42 +0200

    minor changes

--- bioeticgoods/pages/api/email.js
+++ bioeticgoods/pages/api/email.js
@@ -7,7 +7,7 @@ export default (req, res) => {
   try {
     const message = {
       to: 'iliailia@me.com',
-      from: 'iliailia@me.com',
+      from: 'me@bioeticgoods.iliaroger.de',
       subject: data.inquiry,
       text: `First Name: ${data.firstName} \nLast Name: ${data.lastName} \nEmail: ${data.email} \nPhone: ${data.phone} \nMessage: ${data.text}`,
     };
