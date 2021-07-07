3d11ed39bca8106d3240210204a74942f64de2b1
Ilia Roger <iliailia@me.com>
2021-07-07 11:46:47 +0200

    send grid change

--- bioeticgoods/pages/api/email.js
+++ bioeticgoods/pages/api/email.js
@@ -3,6 +3,9 @@ const mail = require('@sendgrid/mail');
 mail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export default (req, res) => {
+  {
+    //add email settings
+  }
   const data = req.body;
   try {
     const message = {
