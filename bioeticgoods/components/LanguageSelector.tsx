8ec35973fbdc2ea3f8cf050437ab625d9bb01c64
Ilia Roger <iliailia@me.com>
2021-08-31 21:48:47 +0200

    minor changes

--- bioeticgoods/components/LanguageSelector.tsx
+++ bioeticgoods/components/LanguageSelector.tsx
@@ -3,6 +3,7 @@ import { ChevronDownIcon } from '@heroicons/react/solid';
 import { Fragment, useEffect } from 'react';
 import { useRouter } from 'next/router';
 
+// russian
 const languages = [
   {
     name: 'English',
