c4a4a7ec66116d188b5780ab6a85a5fea0029cc8
Ilia Roger <iliailia@me.com>
2021-09-01 11:59:49 +0200

    minor changes

--- bioeticgoods/components/LanguageSelector.tsx
+++ bioeticgoods/components/LanguageSelector.tsx
@@ -49,11 +49,11 @@ export default function LanguageSelector() {
           >
             <img
               className="w-4 mr-1 select-none"
-              src="/images/united.png"
+              src="/images/russia.png"
               alt="united kingdom"
             ></img>
             <span className="font-light md:text-xs lg:text-base select-none">
-              English
+              Pусский
             </span>
             <ChevronDownIcon
               className={`${open ? '' : 'text-opacity-70'}
