3450c6cb04f8d7238c71a184b6598ef0f43458f5
Ilia Roger <iliailia@me.com>
2021-10-27 17:11:14 +0200

    added new images to partners

--- bioeticgoods/components/Partners.tsx
+++ bioeticgoods/components/Partners.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 export default function Partners() {
   return (
-    <div className="h-48 flex flex-row items-center">
-      <div className="flex xsm:flex-col lg:flex-row w-5/6 mx-auto justify-center items-center gap-x-12">
+    <div className="flex flex-row items-center my-10">
+      <div className="flex xsm:flex-col lg:flex-col w-5/6 mx-auto justify-center items-center gap-x-12">
         <p className="text-gray-600 font-light xsm:text-sm lg:text-lg tracking-wide text-center">
           Our suppliers who deliver quality food
         </p>
-        <div className="flex flex-row items-center xsm:gap-x-4 md:gap-x-10 lg:gap-x-8 xsm:mt-2 lg:mt-0">
+        <div className="flex flex-row flex-wrap justify-center items-center xsm:gap-x-6 md:gap-x-10 lg:gap-x-8 xsm:mt-2 lg:mt-0 gap-y-4">
           <a
             target="_blank"
             href="https://firmaitalia.it/en/"
@@ -57,6 +57,55 @@ export default function Partners() {
               className="w-24 cursor-pointer transition duration-300 filter grayscale hover:filter hover:grayscale-0"
               src="/images/supplier5.png"
               alt="supplier5"
+            ></img>
+          </a>
+          <a target="_blank" href="http://verdeorosrl.it/">
+            <img
+              className="w-24 cursor-pointer transition duration-300 filter grayscale hover:filter hover:grayscale-0"
+              src="/images/supplier7.png"
+              alt="supplier6"
+            ></img>
+          </a>
+          <a target="_blank" href="http://verdeorosrl.it/">
+            <img
+              className="w-24 cursor-pointer transition duration-300 filter grayscale hover:filter hover:grayscale-0"
+              src="/images/supplier8.png"
+              alt="supplier7"
+            ></img>
+          </a>
+          <a target="_blank" href="http://verdeorosrl.it/">
+            <img
+              className="w-24 cursor-pointer transition duration-300 filter grayscale hover:filter hover:grayscale-0"
+              src="/images/supplier9.png"
+              alt="supplier8"
+            ></img>
+          </a>
+          <a target="_blank" href="http://verdeorosrl.it/">
+            <img
+              className="w-24 cursor-pointer transition duration-300 filter grayscale hover:filter hover:grayscale-0"
+              src="/images/supplier10.png"
+              alt="supplier9"
+            ></img>
+          </a>
+          <a target="_blank" href="http://verdeorosrl.it/">
+            <img
+              className="w-24 cursor-pointer transition duration-300 filter grayscale hover:filter hover:grayscale-0"
+              src="/images/supplier11.png"
+              alt="supplier10"
+            ></img>
+          </a>
+          <a target="_blank" href="http://verdeorosrl.it/">
+            <img
+              className="w-24 cursor-pointer transition duration-300 filter grayscale hover:filter hover:grayscale-0"
+              src="/images/supplier12.png"
+              alt="supplier11"
+            ></img>
+          </a>
+          <a target="_blank" href="http://verdeorosrl.it/">
+            <img
+              className="w-24 cursor-pointer transition duration-300 filter grayscale hover:filter hover:grayscale-0"
+              src="/images/supplier13.png"
+              alt="supplier12"
             ></img>
           </a>
         </div>
